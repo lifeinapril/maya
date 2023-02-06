683000a40a09a16f7c45c13b4ed83d92e8fabd4a
Abraham <brhmdeen@gmail.com>
2023-02-06 21:49:17 +0100

    x

--- src/components/Items/MayaInput.js
+++ src/components/Items/MayaInput.js
@@ -1,5 +1,4 @@
 import {React, useState } from 'react';
-
 import "../../css/MayaInput.css";
 import {Form, Container
 } from 'react-bootstrap';
@@ -11,14 +10,12 @@ const MayaInput = (props) => {
         e.preventDefault();
         fetch('http://localhost:8000/search',{
         method: 'POST',
-        body: JSON.stringify({
-                    term: input,
-                    a_id: Math.random().toString(36).slice(2),}),
+        body: JSON.stringify({text: input}),
         headers: {'Content-type': 'application/json; charset=UTF-8',}
         })
         .then((data) => {
-            //
-           console.log("searching.......");
+            console.log(data);
+            //something has to happen after result/data is fetched {response,text,intent}
         })
         .catch((err) => {
            console.log(err.message);
@@ -28,7 +25,7 @@ const MayaInput = (props) => {
 return (
     <Container style={{margin:"auto"}}>
         <Form onSubmit={search}>
-            <Form.Control className="maya-input" value={input} onSubmit={search} onChange={e => setInput(e.target.value)} placeholder="What can i do for you today?" />
+            <Form.Control className="maya-input" value={input} onSubmit={search} onChange={e => setInput(e.target.value)} placeholder="Ask me anything" />
         </Form>
     </Container>
 )
